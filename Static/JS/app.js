f421f8c458bb82987500a8aa07b3a4ca8cc80099
M1Bren <blaw908@gmail.com>
2020-01-30 18:02:58 -0500

    update

--- Static/JS/app.js
+++ Static/JS/app.js
@@ -9,17 +9,14 @@ function buildMetadata(sample) {
     panelSelect.html("")
 
     // Use `Object.entries` to add each key and value pair to the panel
-    Object.defineProperties(response).forEach(([key, value])) => {
+    Object.defineProperties(response).forEach(([key, value]) => {
       panelSelect.append("p")
-      .text(`${key}:${value}`);  // tags for each key-value in the metadata.
-    }
+        .text(`${key}:${value}`)  // tags for each key-value in the metadata.
+    });
 
     // Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+    buildGauge(data.WFREQ);
   })
-
-    
-
 
 };
 
@@ -28,50 +25,59 @@ function buildCharts(sample) {
   // Use `d3.json` to fetch the sample data for the plots
   let url = `/samples/${sample}`;
 
-    // Generate the data for the charts using the response
-    d3.json(url).then(response => {
-      let xValue = response['otu_ids'];
-      let yValue = response['sample_values'];
-      let sizeValue = response['sample_values'];
-      let label = response['otu_labels'];
+  // Generate the data for the charts using the response
+  d3.json(url).then(response => {
+    let xValue = response['otu_ids'];
+    let yValue = response['sample_values'];
+    let sizeValue = response['sample_values'];
+    let label = response['otu_labels'];
 
-      let trace1 = {
-        x: xValue,
-        y: yValue,
-        mode: 'markers',
-        marker: {
-          size: sizeValue,
-          color: xValue,
-          colorscale: 'Portland',
-          labels: label,
-          type: 'scatter',  // Makes a combo bubble and scatter chart
-          opacity: 0.5
-        }
+    let trace1 = {
+      x: xValue,
+      y: yValue,
+      mode: 'markers',
+      marker: {
+        size: sizeValue,
+        color: xValue,
+        colorscale: 'Portland',
+        labels: label,
+        type: 'scatter',  // Makes a combo bubble and scatter chart
+        opacity: 0.5
       }
-    });
+    }
+  });
 
-    // Define the specs and build the Bubble Scatter Plot
-    let trace1 = [trace1]    // Specify that trace1 should be in list form
-    
-    let layout = {
-      title: 'Marker Size',
-      xAxis: {title: 'OTU ID'},
-      showlegend: true
-    };
+  // Define the specs and build the Bubble Scatter Plot
+  let trace1 = [trace1]    // Specify that trace1 should be in list form
+
+  let layout = {
+    title: 'Marker Size',
+    xAxis: { title: 'OTU ID' },
+    showlegend: true
+  };
 
 
-    Plotly.newPlot("bubble", trace1, layout)
+  Plotly.newPlot("bubble", trace1, layout)
 
-    // Build a Pie Chart according to specs (Top 10 Samples)
-    let trace2 = [{
-      values: sizeValue.splice(0,10),
-      labels: xValue.splice(0,10),
-      text: yValue.splice(0,10),
-      type: 'pie'
-    }];
+  // Build a Pie Chart according to specs (Top 10 Samples)
+  let trace2 = [{
+    values: sizeValue.splice(0, 10),
+    labels: xValue.splice(0, 10),
+    text: yValue.splice(0, 10),
+    type: 'pie'
+  }];
 
-    Plotly.newPlot('pie', trace2);
+  Plotly.newPlot('pie', trace2);
 }
+
+function buildGauge(data) {
+  // Define the pointer range between 0 and 180 degrees
+  let range = parseFloat(data) * 20;
+
+  // Calculate the meter point
+  let degree
+}
+
 
 function init() {
   // Grab a reference to the dropdown select element
